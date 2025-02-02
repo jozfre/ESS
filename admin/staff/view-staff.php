40cb72359ce0738dde5d8af5acbc5e3fc5722f80
jozfre <johanzafrijohar@gmail.com>
2025-02-03 04:10:10 +0800

    view staff details backend logic update

--- admin/staff/view-staff.php
+++ admin/staff/view-staff.php
@@ -1,25 +1,23 @@
 <?php
 session_start();
-if(!isset($_SESSION['userlogged']) || ($_SESSION['userlogged'] != 1))
-{
-    header("Location: ../../index.php");
+if (!isset($_SESSION['userlogged']) || ($_SESSION['userlogged'] != 1)) {
+  header("Location: ../../index.php");
 }
 
-if(!isset($_SESSION['userID']))
-{
-    header("Location: ../../php/logout.php");
+if (!isset($_SESSION['userID'])) {
+  header("Location: ../../php/logout.php");
 }
 
 include "../../php/dbconn.php";
 
 //sql to retrieve data for this user
-if(isset($_GET['userID'])){
+if (isset($_GET['userID'])) {
   $userID = mysqli_real_escape_string($conn, $_GET['userID']);
   $sql = "SELECT * FROM user WHERE userID='$userID'";
-  $result = mysqli_query($conn,$sql);
-  if(mysqli_num_rows($result) > 0){
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
     $user = mysqli_fetch_assoc($result);
-  }else {
+  } else {
     die(mysqli_error($conn));
   }
 }
@@ -27,6 +25,7 @@ if(isset($_GET['userID'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -40,232 +39,236 @@ if(isset($_GET['userID'])){
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4 d-flex flex-column" style="height: 100vh;">
-    <!-- Brand Logo -->
-    <a class="brand-link">
-      <img src="../../images/logo-mbtho.png" alt="logo-mbtho" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light text-wrap d-none d-sm-inline text-wrap" style="white-space: normal;" title="Event Scheduling System (ESS)">ESS</span>
-    </a>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 d-flex flex-column" style="height: 100vh;">
+      <!-- Brand Logo -->
+      <a class="brand-link">
+        <img src="../../images/logo-mbtho.png" alt="logo-mbtho" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light text-wrap d-none d-sm-inline text-wrap" style="white-space: normal;" title="Event Scheduling System (ESS)">ESS</span>
+      </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar d-flex flex-column flex-grow-1">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../../images/user-icon.png" class="img-circle elevation-2" alt="User Image">
+      <!-- Sidebar -->
+      <div class="sidebar d-flex flex-column flex-grow-1">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="../../images/user-icon.png" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="#" class="d-block text-truncate"><?php if (isset($_SESSION['name'])) {
+                                                        echo $_SESSION['name'];
+                                                      } ?></a>
+            <a href="#" class="d-block">ADMIN</a>
+          </div>
         </div>
-        <div class="info">
-          <a href="#" class="d-block text-truncate"><?php if(isset($_SESSION['name'])) { echo $_SESSION['name']; } ?></a>
-          <a href="#" class="d-block">ADMIN</a>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2 flex-grow-1">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="../dashboard.php" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                  Dashboard
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="../schedule/view-schedule.php" class="nav-link">
+                <i class="nav-icon fas fa-calendar-alt"></i>
+                <p>
+                  Schedule
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="../event/list-event.php" class="nav-link">
+                <i class="nav-icon fas fa-ticket-alt"></i>
+                <p>
+                  Event
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="../request/list-request.php" class="nav-link">
+                <i class="nav-icon fas fa-file-alt"></i>
+                <p>
+                  Request
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="../mosque-space/list-space.php" class="nav-link">
+                <i class="nav-icon fas fa-mosque"></i>
+                <p>
+                  Mosque Spaces
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="list-staff.php" class="nav-link active">
+                <i class="nav-icon far fa-address-card"></i>
+                <p>
+                  Staff
+                </p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+        <div class="mt-auto mb-3">
+          <a href="../../php/logout.php" class="btn btn-danger btn-block text-white">
+            <i class="nav-icon fas fa-sign-out-alt"></i> Log Out
+          </a>
         </div>
       </div>
+      <!-- /.sidebar -->
+    </aside>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2 flex-grow-1">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="../dashboard.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../schedule/view-schedule.php" class="nav-link">
-              <i class="nav-icon fas fa-calendar-alt"></i>
-              <p>
-                Schedule
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../event/list-event.php" class="nav-link">
-              <i class="nav-icon fas fa-ticket-alt"></i>
-              <p>
-                Event
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../request/list-request.php" class="nav-link">
-              <i class="nav-icon fas fa-file-alt"></i>
-              <p>
-                Request
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../mosque-space/list-space.php" class="nav-link">
-              <i class="nav-icon fas fa-mosque"></i>
-              <p>
-                Mosque Spaces
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="list-staff.php" class="nav-link active">
-              <i class="nav-icon far fa-address-card"></i>
-              <p>
-                Staff
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-      <div class="mt-auto mb-3">
-            <a href="../../php/logout.php" class="btn btn-danger btn-block text-white">
-                <i class="nav-icon fas fa-sign-out-alt"></i> Log Out
-            </a>
-        </div>
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Staff</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Staff</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="list-staff.php">List of Staff</a></li>
                 <li class="breadcrumb-item active">View Staff Details</li>
-            </ol>
-          </div>
-          <!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-12">
-            <!-- general form elements -->
-            <div class="card card-dark">
-              <div class="card-header">
-                <h3 class="card-title">Staff Details</h3>
-                <div class="d-flex justify-content-end">
-
-                <?php if($user['name'] != ($_SESSION['name'])) : ?>
-                
-                <?php else : ?>
-                    <button type="button" class="btn btn-danger btn-sm float-right mr-2" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash-alt"></i> Delete Staff</button>
-                    <button type="button" class="btn btn-primary btn-sm float-right" onclick="location.href='update-staff.php'"><i class="fas fa-edit"></i> Update Details</button>
-                <?php endif; ?> 
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="userID">Staff ID</label>
-                    <input name="userID" class="form-control" id="userID" value="<?= $user['userID'];?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label for="name">Staff Name</label>
-                    <input name="name" class="form-control" id="name" value="<?= $user['name'];?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label for="telNum">Telephone Number</label>
-                    <input name="telNum" class="form-control" id="telNum" value="<?= $user['telNum'];?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email</label>
-                    <input name="email" class="form-control" id="email" value="<?= $user['email'];?>" readonly>
-                  </div>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer d-flex justify-content-between align-items-center">
-                  <button type="button" class="btn btn-dark" onclick="location.href='list-staff.php'">Back</button>
-                </div>
-              </form>
+              </ol>
             </div>
-            <!-- /.card -->
-          </div>
-          <!--/.col (left) -->
-          <!-- right column -->
-          <!--/.col (right) -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-</div>
-<!-- ./wrapper -->
+            <!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
 
-<!-- Deletion Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="deleteModalLabel">Delete Staff</h5>
-      </div>
-      <div class="modal-body">
-        Are you sure you want to delete the following staff?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
-        <button type="button" class="btn btn-primary">Confirm</button>
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+              <!-- general form elements -->
+              <div class="card card-dark">
+                <div class="card-header">
+                  <h3 class="card-title">Staff Details</h3>
+                  <div class="d-flex justify-content-end">
+
+                    <?php if ($user['name'] != ($_SESSION['name'])) : ?>
+
+                    <?php else : ?>
+                      <button type="button" class="btn btn-danger btn-sm float-right mr-2" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash-alt"></i> Delete Staff</button>
+                      <button type="button" class="btn btn-primary btn-sm float-right" onclick="location.href='update-staff.php'"><i class="fas fa-edit"></i> Update Details</button>
+                    <?php endif; ?>
+                  </div>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form>
+                  <div class="card-body">
+                    <div class="form-group">
+                      <label for="userID">Staff ID</label>
+                      <input name="userID" class="form-control" id="userID" value="<?= $user['userID']; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="name">Staff Name</label>
+                      <input name="name" class="form-control" id="name" value="<?= $user['name']; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="telNum">Telephone Number</label>
+                      <input name="telNum" class="form-control" id="telNum" value="<?= $user['telNum']; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="email">Email</label>
+                      <input name="email" class="form-control" id="email" value="<?= $user['email']; ?>" readonly>
+                    </div>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn btn-dark" onclick="location.href='list-staff.php'">Back</button>
+                  </div>
+                </form>
+              </div>
+              <!-- /.card -->
+            </div>
+            <!--/.col (left) -->
+            <!-- right column -->
+            <!--/.col (right) -->
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- Deletion Modal -->
+  <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="deleteModalLabel">Delete Staff</h5>
+        </div>
+        <div class="modal-body">
+          Are you sure you want to delete the following staff?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
+          <button type="button" class="btn btn-primary">Confirm</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<!-- End of Deletion Modal -->
+  <!-- End of Deletion Modal -->
 
-<!-- REQUIRED SCRIPTS -->
+  <!-- REQUIRED SCRIPTS -->
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- bs-custom-file-input -->
-<script src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- Page specific script -->
-<script>
-  $(function () {
-  bsCustomFileInput.init();
-});
-</script>
+  <!-- jQuery -->
+  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- bs-custom-file-input -->
+  <script src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/adminlte.min.js"></script>
+  <!-- Page specific script -->
+  <script>
+    $(function() {
+      bsCustomFileInput.init();
+    });
+  </script>
 </body>
+
 </html>
