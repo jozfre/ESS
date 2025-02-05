664c62c093e036661b9c782a9dddd2077601c9e8
jozfre <johanzafrijohar@gmail.com>
2025-02-05 22:48:38 +0800

    delete/cancel request backend logic

---
+++ php/delete-request.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include "dbconn.php";
+
+if (isset($_POST['deleteRequest'])) {
+  $requestID = mysqli_real_escape_string($conn, $_POST['requestID']);
+
+  $sql = "UPDATE request SET isDeleted = 1 WHERE requestID = ?";
+  $stmt = mysqli_prepare($conn, $sql);
+  mysqli_stmt_bind_param($stmt, "s", $requestID);
+
+  if (mysqli_stmt_execute($stmt)) {
+    $_SESSION['success'] = "Event request cancelled successfully";
+    header("Location: ../organizer/request/list-request.php");
+  } else {
+    $_SESSION['error'] = "Error cancelling event request: " . mysqli_error($conn);
+    header("Location: ../organizer/request/view-request.php?requestID=" . $requestID);
+  }
+  exit();
+}
+?>
